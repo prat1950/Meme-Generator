ca4bca0f24890b96594d2b1cb2b5365aeed35c18
Pratik Senapati <147714325+prat1950@users.noreply.github.com>
2023-10-21 13:38:33 +0530

    Image retrieval
    Add js data image retrieval

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,12 +1,32 @@
 import React from "react";
+import memesData from "../memesData.js"
 
 export default function Meme() {
+    
+
+
+    const [img, func]=React.useState("");
+
+    
+
+    function getMeme(){
+        const memesArray=memesData.data.memes;
+        const randNum=Math.floor(Math.random()*memesArray.length);
+        func(memesArray[randNum].url)
+    }
+    
+    
     return (
-        <div className="grid grid-cols-2 grid-rows-2 gap-4 mt-16 w-4/5 mx-auto">
-        <input type="text" className="col-span-1 row-span-1 p-2 border border-gray-300 rounded-md" />
-        <input type="text" className="col-span-1 row-span-1 p-2 border border-gray-300 rounded-md" />
-        <button className="col-span-2 row-span-1 p-2 bg-blue-500 text-white rounded-md bg-gradient font-karla font-bold">Generate A new Meme 🖼 </button>
-    </div>
+        <div>
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 mt-16 w-4/5 mx-auto">
+            <input type="text" className="col-span-1 row-span-1 p-2 border border-gray-300 rounded-md" placeholder="Top Text" />
+            <input type="text" className="col-span-1 row-span-1 p-2 border border-gray-300 rounded-md" placeholder="Bottom Text"/>
+            <button className="col-span-2 row-span-1 p-2 mx-auto bg-blue-500 text-white rounded-md bg-gradient font-karla font-bold w-4/5" onClick={getMeme}>Generate A new Meme 🖼 </button>
+            </div>
+            <div>
+                <img src={img} alt="your meme" className="w-4/5 mx-auto mt-12"/>
+            </div>
+        </div>
 
     )
 }
