6490a5370d9751facf5079935451f0bf6ab52a49
Pratik Senapati <147714325+prat1950@users.noreply.github.com>
2023-10-25 18:27:49 +0530

    Add top and bottom text to images

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -5,26 +5,55 @@ export default function Meme() {
     
 
 
-    const [img, func]=React.useState("");
 
+    const [meme, setMeme ]=React.useState({
+        topText:"",
+        bottomText:"",
+        randImg:"http://i.imgflip.com/1bij.jpg"
+    });
     
+    const [allMemes, setAllMemes]=React.useState(memesData);
+
 
     function getMeme(){
         const memesArray=memesData.data.memes;
         const randNum=Math.floor(Math.random()*memesArray.length);
-        func(memesArray[randNum].url)
-    }
+        const url=memesArray[randNum].url
+        setMeme(prevMeme=> ({
+            ...prevMeme,
+            randImg:url
+        }))
+        } ;
     
+    function handleChange(event){
+        const {name, value}=event.target;
+        setMeme((prevMeme)=>({
+            ...prevMeme,
+            [name]:value
+        }))
+    } 
+
     
     return (
         <div>
             <div className="grid grid-cols-2 grid-rows-2 gap-4 mt-16 w-4/5 mx-auto">
-            <input type="text" className="col-span-1 row-span-1 p-2 border border-gray-300 rounded-md" placeholder="Top Text" />
-            <input type="text" className="col-span-1 row-span-1 p-2 border border-gray-300 rounded-md" placeholder="Bottom Text"/>
+            <input type="text" className="col-span-1 row-span-1 p-2 border border-gray-300 rounded-md" placeholder="Top Text"
+            name="topText"
+            value={meme.topText} 
+            onChange={handleChange}
+            />
+            <input type="text" className="col-span-1 row-span-1 p-2 border border-gray-300 rounded-md" placeholder="Bottom Text" 
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
+            
+            />
             <button className="col-span-2 row-span-1 p-2 mx-auto bg-blue-500 text-white rounded-md bg-gradient font-karla font-bold w-4/5" onClick={getMeme}>Generate A new Meme 🖼 </button>
             </div>
-            <div>
-                <img src={img} alt="your meme" className="w-4/5 mx-auto mt-12"/>
+            <div className="relative">
+                <h2 className="absolute inset-x-0 top-5 text-center font-karla text-white font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">{meme.topText}</h2>
+                <h2 className="absolute inset-x-0 bottom-5 text-center font-karla text-white font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">{meme.bottomText}</h2>
+                <img src={meme.randImg} alt="your meme" className="w-4/5 mx-auto mt-12"/>
             </div>
         </div>
 
